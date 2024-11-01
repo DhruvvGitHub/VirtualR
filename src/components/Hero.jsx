46b456208394dcb66770636aaa07f170d337c237
DhruvvGitHub <dhruvtanwani1915@gmail.com>
2024-11-02 00:09:39 +0530

    changes in responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import video2 from "../assets/video2.mp4";
 
 const Hero = () => {
   return (
-    <div className="hero w-full h-[110vh]">
+    <div className="hero w-full min-h-[110vh] px-4">
       <div className="container flex flex-col gap-12 items-center pt-16">
         <h1 className="text-4xl sm:text-6xl lg:text-8xl text-center">
           VirtualR builds tools <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">for developers</span>
@@ -23,9 +23,9 @@ const Hero = () => {
           </a>
         </div>
 
-        <div className="videos flex items-center gap-10">
-            <video className="w-100 h-80 overflow-hidden rounded-md border-2 border-orange-500 object-cover"  loop autoPlay muted src={video1}></video>
-            <video className="w-100 h-80 overflow-hidden rounded-md border-2 border-orange-500 object-cover"  loop autoPlay muted src={video2}></video>
+        <div className="videos flex flex-col sm:flex-row items-center gap-10">
+            <video className="w-full sm:w-100 h-80 overflow-hidden rounded-md border-2 border-orange-500 object-cover"  loop autoPlay muted src={video1}></video>
+            <video className="w-full sm:w-100 h-80 overflow-hidden rounded-md border-2 border-orange-500 object-cover"  loop autoPlay muted src={video2}></video>
         </div>
       </div>
     </div>
